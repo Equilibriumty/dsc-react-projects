069d8800f07d84c94149dee8784d4ec8cf47b0f4
Equilibriumty <fanflackplay@gmail.com>
2021-04-18 16:44:08 +0300

    test get request

--- 03-ip-adress-tracker/code/src/App.js
+++ 03-ip-adress-tracker/code/src/App.js
@@ -1,8 +1,26 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Map } from "./components/Map";
+import { useEffect } from "react";
 
 function App() {
+  const ip = "46.219.209.158";
+  const api_key = "at_O7ACF17wFxNr9UlF3Z0LOb1NREykS";
+  const api_url = "https://geo.ipify.org/api/v1?";
+  const url = api_url + "apiKey=" + api_key + "&ipAddress=" + ip;
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, [url]);
   return (
     <div>
       <Header />
