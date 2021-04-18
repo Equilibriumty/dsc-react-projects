e2b6d75ef066aa3bcdf82a22066bac93099e4e1c
Equilibriumty <fanflackplay@gmail.com>
2021-04-18 19:44:14 +0300

    minor fixes

--- 03-ip-adress-tracker/code/src/components/Header.js
+++ 03-ip-adress-tracker/code/src/components/Header.js
@@ -3,13 +3,14 @@ import "../styles/Header.css";
 export const Header = () => {
   return (
     <div className="header-body">
-      <div className="header-text">
-        Ip Adress Tracker
-      </div>
-      <form class="search-wrapper cf">
-        <input type="text" placeholder="Search for any IP address or domain" required></input>
+      <div className="header-text">IP Address Tracker</div>
+      <form className="search-wrapper cf">
+        <input
+          type="text"
+          placeholder="Search for any IP address or domain"
+          required
+        ></input>
         <button type="submit">{">"}</button>
-     
       </form>
     </div>
   );
--- 03-ip-adress-tracker/code/src/components/Info.js
+++ 03-ip-adress-tracker/code/src/components/Info.js
@@ -1,42 +1,26 @@
-import "../styles/Info.css"
+import "../styles/Info.css";
 
-const Info = ()=>{
-    return(
-        <div className="info-body">
-            <div className="info-elem">
-                <div className="info-header">
-                    Ip Address
-                </div>
-                <div className="info-text">
-                    192.212.174.101
-                </div>
-            </div>
-            <div className="info-elem">
-                <div className="info-header">
-                    Location
-                </div>
-                <div className="info-text">
-                    Brooklyn, NY 10001
-                </div>
-            </div>
-            <div className="info-elem">
-                <div className="info-header">
-                    Timezone
-                </div>
-                <div className="info-text">
-                    UTC-05:00
-                </div>
-            </div>
-            <div className="info-elem">
-                <div className="info-header">
-                    Isp
-                </div>
-                <div className="info-text">
-                    SpaceX Starlink
-                </div>
-            </div>
-        </div>
-    )
-}
+const Info = () => {
+  return (
+    <div className="info-body">
+      <div className="info-elem">
+        <div className="info-header">Ip Address</div>
+        <div className="info-text">192.212.174.101</div>
+      </div>
+      <div className="info-elem">
+        <div className="info-header">Location</div>
+        <div className="info-text">Brooklyn, NY 10001</div>
+      </div>
+      <div className="info-elem">
+        <div className="info-header">Timezone</div>
+        <div className="info-text">UTC-05:00</div>
+      </div>
+      <div className="info-elem">
+        <div className="info-header">Isp</div>
+        <div className="info-text">SpaceX Starlink</div>
+      </div>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
